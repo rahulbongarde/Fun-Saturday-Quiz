90e3d1eed748b58bc7aa6af8fc5af3b506a90718
rahulbongarde <rahulbongarde0@gmail.com>
2026-06-19 20:50:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+let current=0;
+let scoreA=0;
+let scoreB=0;
+let timer;
 let time=10;
 
-function startTimer(){
+function startQuiz(){
+loadQuestion();
+}
+
+function loadQuestion(){
+
+document.getElementById("question").innerHTML=
+questions[current].question;
+
+document.getElementById("answer").style.display="none";
 
 time=10;
 
-let x=setInterval(()=>{
+clearInterval(timer);
+
+timer=setInterval(()=>{
 
 document.getElementById("timer").innerHTML=time;
 
@@ -12,12 +27,47 @@ time--;
 
 if(time<0){
 
-clearInterval(x);
+clearInterval(timer);
 
 document.getElementById("answer").style.display="block";
+
+document.getElementById("answer").innerHTML=
+"✅ "+questions[current].answer;
 
 }
 
 },1000);
+
+}
+
+function nextQuestion(){
+
+current++;
+
+if(current>=questions.length){
+
+alert("Quiz Completed");
+
+current=0;
+
+}
+
+loadQuestion();
+
+}
+
+function addScoreA(){
+
+scoreA++;
+
+document.getElementById("scoreA").innerHTML=scoreA;
+
+}
+
+function addScoreB(){
+
+scoreB++;
+
+document.getElementById("scoreB").innerHTML=scoreB;
 
 }
