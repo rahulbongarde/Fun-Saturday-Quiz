8d94fc971a5c54a1ca93549e48f580fb342568da
rahulbongarde <rahulbongarde0@gmail.com>
2026-06-19 17:25:02 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,53 @@
+const questions = [
+
+{
+question:"👂 + 🐦",
+answer:"Earbud"
+},
+
+{
+question:"🔥 + 🦊",
+answer:"Firefox"
+},
+
+{
+question:"📖 + 🪱",
+answer:"Bookworm"
+},
+
+{
+question:"🌍 + 🕸️",
+answer:"World Wide Web"
+},
+
+{
+question:"💻 + 🐁",
+answer:"Computer Mouse"
+},
+
+{
+question:"🏠 + 🎭",
+answer:"Home Theatre"
+},
+
+{
+question:"🚗 + 📷",
+answer:"Dashcam"
+},
+
+{
+question:"🎤 + 🎧",
+answer:"Headset"
+},
+
+{
+question:"☁️ + 💾",
+answer:"Cloud Storage"
+},
+
+{
+question:"🧠 + 🌩️",
+answer:"Brainstorm"
+}
+
+];
