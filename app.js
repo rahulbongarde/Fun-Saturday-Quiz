b5384f1706b016a70c2a23dbd06b71cf2498285a
rahulbongarde <rahulbongarde0@gmail.com>
2026-06-20 09:47:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
 
-let current = 0;
-let scoreA = 0;
-let scoreB = 0;
-let timer;
-let time = 10;
-
-function startQuiz() {
-
-    loadQuestion();
-
-}
-
 function loadQuestion() {
-
-    console.log(window.questions);
 
     document.getElementById("question").innerHTML =
         window.questions[current].question;
-
-}
 
     document.getElementById("answer").style.display = "none";
 
@@ -51,45 +35,4 @@ function loadQuestion() {
     }, 1000);
 
 }
-
-function nextQuestion() {
-
-    current++;
-
-    if (current >= window.questions.length) {
-
-        current = 0;
-
-    }
-
-    loadQuestion();
-
-}
-
-function addScoreA() {
-
-    scoreA++;
-
-    document.getElementById("scoreA").innerHTML = scoreA;
-
-    document.getElementById("winner").innerHTML =
-        "🏆 Winner : Team A";
-
-}
-
-function addScoreB() {
-
-    scoreB++;
-
-    document.getElementById("scoreB").innerHTML = scoreB;
-
-    document.getElementById("winner").innerHTML =
-        "🏆 Winner : Team B";
-
-}
-
-window.startQuiz = startQuiz;
-window.nextQuestion = nextQuestion;
-window.addScoreA = addScoreA;
-window.addScoreB = addScoreB;
 ```
