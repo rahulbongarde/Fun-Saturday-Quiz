341d8cb71d24becd94bc19de56724105746a47f2
rahulbongarde <rahulbongarde0@gmail.com>
2026-06-20 09:24:33 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,54 +1,42 @@
-
 window.questions = [
-
-{
-question:"👂 + 🐦",
-answer:"Earbud"
-},
-
-{
-question:"🔥 + 🦊",
-answer:"Firefox"
-},
-
-{
-question:"📖 + 🪱",
-answer:"Bookworm"
-},
-
-{
-question:"🌍 + 🕸️",
-answer:"World Wide Web"
-},
-
-{
-question:"💻 + 🐁",
-answer:"Computer Mouse"
-},
-
-{
-question:"🏠 + 🎭",
-answer:"Home Theatre"
-},
-
-{
-question:"🚗 + 📷",
-answer:"Dashcam"
-},
-
-{
-question:"🎤 + 🎧",
-answer:"Headset"
-},
-
-{
-question:"☁️ + 💾",
-answer:"Cloud Storage"
-},
-
-{
-question:"🧠 + 🌩️",
-answer:"Brainstorm"
-}
-
+    {
+        question: "👂 + 🐦",
+        answer: "Earbud"
+    },
+    {
+        question: "🔥 + 🦊",
+        answer: "Firefox"
+    },
+    {
+        question: "📖 + 🪱",
+        answer: "Bookworm"
+    },
+    {
+        question: "🌍 + 🕸️",
+        answer: "World Wide Web"
+    },
+    {
+        question: "💻 + 🐁",
+        answer: "Computer Mouse"
+    },
+    {
+        question: "🏠 + 🎭",
+        answer: "Home Theatre"
+    },
+    {
+        question: "🚗 + 📷",
+        answer: "Dashcam"
+    },
+    {
+        question: "🎤 + 🎧",
+        answer: "Headset"
+    },
+    {
+        question: "☁️ + 💾",
+        answer: "Cloud Storage"
+    },
+    {
+        question: "🧠 + 🌩️",
+        answer: "Brainstorm"
+    }
 ];
