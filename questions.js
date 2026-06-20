9d4351c13d19da8884e19eb8edf2658a826cdf8b
rahulbongarde <rahulbongarde0@gmail.com>
2026-06-20 14:07:31 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,42 +1,48 @@
 window.questions = [
+
 {
-question:"👂 + 🐦",
-answer:"Earbud"
-},
-{
-question:"🔥 + 🦊",
+question: `
+<img src="https://cdn-icons-png.flaticon.com/128/2991/2991594.png" width="80">
++
+<img src="https://cdn-icons-png.flaticon.com/128/616/616554.png" width="80">
+`,
 answer:"Firefox"
 },
+
 {
-question:"📖 + 🪱",
+question: `
+<img src="https://cdn-icons-png.flaticon.com/128/1048/1048941.png" width="80">
++
+<img src="https://cdn-icons-png.flaticon.com/128/616/616430.png" width="80">
+`,
+answer:"Earbud"
+},
+
+{
+question: `
+<img src="https://cdn-icons-png.flaticon.com/128/3145/3145765.png" width="80">
++
+<img src="https://cdn-icons-png.flaticon.com/128/3069/3069172.png" width="80">
+`,
 answer:"Bookworm"
 },
+
 {
-question:"🌍 + 🕸️",
+question: `
+<img src="https://cdn-icons-png.flaticon.com/128/869/869869.png" width="80">
++
+<img src="https://cdn-icons-png.flaticon.com/128/1006/1006771.png" width="80">
+`,
 answer:"World Wide Web"
 },
+
 {
-question:"💻 + 🐁",
+question: `
+<img src="https://cdn-icons-png.flaticon.com/128/3474/3474360.png" width="80">
++
+<img src="https://cdn-icons-png.flaticon.com/128/2965/2965300.png" width="80">
+`,
 answer:"Computer Mouse"
-},
-{
-question:"🏠 + 🎭",
-answer:"Home Theatre"
-},
-{
-question:"🚗 + 📷",
-answer:"Dashcam"
-},
-{
-question:"🎤 + 🎧",
-answer:"Headset"
-},
-{
-question:"☁️ + 💾",
-answer:"Cloud Storage"
-},
-{
-question:"🧠 + 🌩️",
-answer:"Brainstorm"
 }
+
 ];
